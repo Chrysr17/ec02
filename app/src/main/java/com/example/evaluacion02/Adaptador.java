d7bf8ba676e68ad4ad9dbd88b5ec29f37bf43ae2
Chrysr17 <chrisrasaro@gmail.com>
2021-12-10 23:59:56 -0500

    splas

---
+++ app/src/main/java/com/example/evaluacion02/Adaptador.java
@@ -0,0 +1,40 @@
+package com.example.evaluacion02;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+public class Adaptador extends BaseAdapter {
+    private Context context;
+    private ArrayList<Entidad> listItems;
+
+    public Adaptador(Context context, ArrayList<Entidad> listItems) {
+        this.context = context;
+        this.listItems = listItems;
+    }
+
+    @Override
+    public int getCount() {
+        return listItems.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return listItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        return null;
+    }
+}
---
+++ app/src/main/java/com/example/evaluacion02/Entidad.java
@@ -0,0 +1,4 @@
+package com.example.evaluacion02;
+
+public class Entidad {
+}
